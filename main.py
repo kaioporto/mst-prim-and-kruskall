bfa59f17931a741062fc9aecff41888abbad6f1f
Kaio Porto <kaio.rodrigo28@gmail.com>
2023-06-23 20:27:00 -0300

    commit inicial, adicionando passos do que fazer no algoritmo

---
+++ main.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+#graph structure using defaultdict
+#v = {1, 2, 3, 4, 5}
+#e = 
+
+def minimum_spanning_tree():
+	#ordenar as arestas por peso; usar um sort por ordem crescente
+	
+	#ir adicinonando as arestas à arvore
+	#só adicionar aresta que não forma ciclo (pesquisar como checar isso, qual critério usar)
+	#se os vertices incidentes da aresta já foram visitados e possuem vizinhos em comum, isso forma um ciclo
+    pass
+
+def disjoint_sets():
+    pass
+
