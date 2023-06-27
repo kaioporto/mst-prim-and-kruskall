5aff98fe429b3e9bf6fbf690fda797b40cad49d3
Matheus Freitas <msformigteen@live.com>
2023-06-26 23:40:43 -0300

    feat: change some names

--- Program/graph.py
+++ Program/graph.py
@@ -4,6 +4,7 @@ class Graph(object):
             self.neighbor = None
             self.edge = None
             self.next = None
+            self.previous = None
 
     class Edge(object):
         def __init__(self):
@@ -19,71 +20,68 @@ class Graph(object):
             yield i
 
     def get_edges(self, IterararSobreNo=False):
-        for v in self.get_vertices():
-            for w in self.get_neighbors(v, Tipo="+" if self.is_oriented else "*", IterararSobreNo=IterararSobreNo):
+        for a_vertex in self.get_vertices():
+            for w in self.get_neighbors(a_vertex, a_type="+" if self.is_oriented else "*", IterararSobreNo=IterararSobreNo):
                 enumerar = True
                 if not self.is_oriented:
                     wint = w if isinstance(w, int) else w.neighbor
-                    enumerar = v < wint
+                    enumerar = a_vertex < wint
                 if enumerar:
-                    yield (v, w)
+                    yield (a_vertex, w)
 
-    def set_quantity_vertices(self, quantity, has_before_node=False):
+    def set_quantity_vertices(self, quantity, has_previous=False):
         self.quantity_vertices, self.quantity_edges = quantity, 0
         self.nodes = [None] * (self.quantity_vertices + 1)
         for i in range(1, self.quantity_vertices + 1):
             self.nodes[i] = Graph.Node()
-            self.has_before_node = has_before_node
+            self.has_previous = has_previous
         return self
 
-    def RemoverAresta(self, uv):
-        """
-        Remove a aresta uv
-        """
+    def remove_node(self, a_node):
+        a_node.previous.next = a_node.next
+        if a_node.next != None:
+            a_node.next.previous = a_node.previous
 
-        def RemoverLista(a_node):
-            a_node.Ant.next = a_node.next
-            if a_node.next != None:
-                a_node.next.Ant = a_node.Ant
+    def remove_edge(self, an_edge):
+        self.remove_node(an_edge.node_one)
+        self.remove_node(an_edge.node_two)
+        return self
 
-        RemoverLista(uv.node_one)
-        RemoverLista(uv.node_two)
-
-    def add_edge(self, u, v, weight=0):
+    def add_edge(self, an_other_vertex, a_vertex, weight=0):
 
         an_edge = Graph.Edge()
-        an_edge.vertex_one, an_edge.vertex_two, an_edge.weight = u, v, weight
-        an_edge.node_one = self.make_node(u, v, an_edge, "+")
-        an_edge.node_two = self.make_node(v, u, an_edge, "-")
+        an_edge.vertex_one, an_edge.vertex_two, an_edge.weight = an_other_vertex, a_vertex, weight
+        an_edge.node_one = self.make_node(an_other_vertex, a_vertex, an_edge, "+")
+        an_edge.node_two = self.make_node(a_vertex, an_other_vertex, an_edge, "-")
         self.quantity_edges = self.quantity_edges + 1
         return self
 
-    def is_neighbor(self, u, v):
-        Tipo = "+" if self.is_oriented else "*"
-        for w in self.get_neighbors(u, Tipo):
-            if w == v:
+    def is_neighbor(self, an_other_vertex, a_vertex):
+        a_type = "+" if self.is_oriented else "*"
+        for w in self.get_neighbors(an_other_vertex, a_type):
+            if w == a_vertex:
                 return True
             return False
 
-    def get_neighbors(self, v, Tipo="*", Fechada=False, use_node=False):
-        if Fechada:
+    def get_neighbors(self, a_vertex, a_type="*", is_closed=False, use_node=False):
+        if is_closed:
             a_node = Graph.Node()
-            a_node.neighbor, a_node.edge, a_node.next = v, None, None
+            a_node.neighbor, a_node.edge, a_node.next = a_vertex, None, None
             yield a_node if use_node else a_node.neighbor
-        w = self.nodes[v].next
-        while w != None:
-            if Tipo == "*" or w.Tipo == Tipo:
-                yield w if use_node else w.neighbor
-            w = w.next
+        a_next_node = self.nodes[a_vertex].next
+        while a_next_node != None:
+            if a_type == "*" or a_next_node.a_type == a_type:
+                yield a_next_node if use_node else a_next_node.neighbor
+            a_next_node = a_next_node.next
 
-    def make_node(self, an_other_vertex, a_vertex, an_edge, Tipo):
+    def make_node(self, an_other_vertex, a_vertex, an_edge, a_type):
         a_node = Graph.Node()
         a_node.neighbor, a_node.edge, a_node.next, self.nodes[an_other_vertex].next = a_vertex, an_edge, self.nodes[
             an_other_vertex].next, a_node
-        if self.has_before_node:
-            self.nodes[an_other_vertex].next.Ant = self.nodes[an_other_vertex]
+        if self.has_previous:
+            self.nodes[an_other_vertex].next.previous = self.nodes[an_other_vertex]
             if self.nodes[an_other_vertex].next.next != None:
-                self.nodes[an_other_vertex].next.next.Ant = self.nodes[an_other_vertex].next
+                self.nodes[an_other_vertex].next.next.previous = self.nodes[an_other_vertex].next
         if self.is_oriented:
-            a_node.Tipo = Tipo
+            a_node.a_type = a_type
         return a_node
